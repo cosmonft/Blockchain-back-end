5342572447f27c3ac1c7654e7ba4d32ecfbfeca9
gpablo6 <gpablo6@outlook.com>
2022-10-08 19:42:36 -0500

    Fix models according to bd migration

--- src/models/commentMessages.ts
+++ src/models/commentMessages.ts
@@ -2,21 +2,53 @@ import { Sequelize, DataTypes } from 'sequelize';
 
 export const commentMessagesModel = (initializedSequel: Sequelize) => {
     return initializedSequel.define('comment_messages', {
-        MessageID: {
+        messasge_id: {
             type: DataTypes.NUMBER,
             allowNull: false
         },
-        ProfileId: {
+        sent: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true
+        }, 
+        profile_id: {
+            type: DataTypes.NUMBER,
+            allowNull: true
+        },
+        pub_id: {
             type: DataTypes.NUMBER,
             allowNull: false
         },
-        FollowNFT: {
+        content_uri: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: true
         },
-        Timestamp: {
+        profile_id_pointed: {
             type: DataTypes.NUMBER,
-            allowNull: false
+            allowNull: true
+        },
+        pub_id_pointed: {
+            type: DataTypes.NUMBER,
+            allowNull: true
+        },
+        collect_module: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        collect_module_return_data: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        reference_module: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        reference_module_return_data: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        timestamp: {
+            type: DataTypes.NUMBER,
+            allowNull: true
         }
     });
 }
--- src/models/followMessages.ts
+++ src/models/followMessages.ts
@@ -2,53 +2,25 @@ import { Sequelize, DataTypes } from 'sequelize';
 
 export const followMessagesModel = (initializedSequel: Sequelize) => {
     return initializedSequel.define('follow_messages', {
-        MessageID: {
+        message_id: {
             type: DataTypes.NUMBER,
             allowNull: false
         },
-        Sent: {
+        sent: {
             type: DataTypes.BOOLEAN,
-            allowNull: false
-        }, 
-        ProfileId: {
+            allowNull: true
+        },
+        profile_id: {
             type: DataTypes.NUMBER,
-            allowNull: false
+            allowNull: true
         },
-        PubId: {
-            type: DataTypes.NUMBER,
-            allowNull: false
-        },
-        ContentURI: {
+        FollowNFT: {
             type: DataTypes.STRING,
-            allowNull: false
-        },
-        ProfileIdPointed: {
-            type: DataTypes.NUMBER,
-            allowNull: false
-        },
-        PubIdPointed: {
-            type: DataTypes.NUMBER,
-            allowNull: false
-        },
-        CollectModule: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        CollectModuleReturnData: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        ReferenceModule: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        ReferenceModuleReturnData: {
-            type: DataTypes.STRING,
-            allowNull: false
+            allowNull: true
         },
         Timestamp: {
             type: DataTypes.NUMBER,
-            allowNull: false
+            allowNull: true
         }
     });
 }

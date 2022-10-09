1544c0345729c376e3a93d4a2860759b3b8ff9cb
Rodrigo Ramos Xochiteotzin <rodrx20@gmail.com>
2022-10-08 22:52:53 -0500

    server express | added psot endpoing which sends a messege | compile error

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import { sendCommentNotifications } from "./queries";
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+
+app.post('/add-address', (req: Request, res: Response) => {
+    res.send({
+        message: "Sending message to server"
+    });
+})
 
 const main = async () => {
     await sendCommentNotifications();
 } 
 
 main();
+
+app.listen(3000, () => {
+    console.log("server running")
+})
